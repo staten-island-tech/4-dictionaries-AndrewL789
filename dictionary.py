9a8c92074c5adac8c2dd1e8fd0876153118130fa
AndrewL789 <Andrewl434@nycstudents.net>
2025-09-30 09:24:29 -0400

    blah

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+stock= [
+    {
+        "item code": "1",
+        "name":"Xiyang",
+        "price":"1.99",
+        "rating": "2.5/5"
+    
+    },
+    {
+        "item code":"2",
+        "name":"door hinge",
+        "price":"78.99",
+        "rating":"5/5"
+    },
+    {
+        "item code": "3",
+        "name":"Xiyang statue",
+        "price":"23498.99",
+        "rating":"10/5"
+    }
+]
