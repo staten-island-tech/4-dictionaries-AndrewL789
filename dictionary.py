51ad572912f28b42f45aa06f7d1434e95ea21757
AndrewL789 <Andrewl434@nycstudents.net>
2025-10-01 09:23:51 -0400

    huh

--- dictionary.py
+++ dictionary.py
@@ -1,21 +1,44 @@
-stock= [
-    {
-        "item code": "1",
-        "name":"Xiyang",
-        "price":"1.99",
-        "rating": "2.5/5"
-    
+#ratings r outta 5 
+yang= [
+{
+    df
+    'name':'Xiyang Statue',
+    'price':'129.99',
+    'ratings':'8',
+    'desc':'A glorious fully aluminum statue of Xiyang'
+
     },
-    {
-        "item code":"2",
-        "name":"door hinge",
-        "price":"78.99",
-        "rating":"5/5"
+{
+
+    'name':'Xiyang door knob',
+    'price':'5.99',
+    'ratings':'10',
+    'desc':'A brass door knob hand carved into the shape of Xiyangs head'
     },
-    {
-        "item code": "3",
-        "name":"Xiyang statue",
-        "price":"23498.99",
-        "rating":"10/5"
-    }
-]
+{
+    'name':'Xiyang tv remote',
+    'price':'4.99',
+    'ratings':'4',
+    'desc':'A tv remote in the shape of Xiyangs head (batteries not included)'
+    },
+{
+    'name':'Xiyang tapistry'
+    'price':'34.99',
+    'ratings':'9',
+    'desc':'High quality silk tapistry of Xiyang posing dramatically'
+    },
+{
+    'name':'Xiyang soap holder',
+    'price':'11.99',
+    'ratings':'10',
+    'desc':'A plastic carve out of Xiyang with a compartment to store bars of soap'
+},
+    ]
+
+
+for xi in yang:
+    xi['price'] = float(xi['price'])
+    xi['ratings'] = int(xi['ratings'])
+
+
+print(yang['name'])
